b4b682964fda0d0cf8cd1117492dfb71b165d8de
William Zhong <wz16874n@pace.edu>
2019-09-21 13:45:44 -0400

    First Commit

---
+++ crypto_scraper.py
@@ -0,0 +1,7 @@
+# Makes a request to https://coinmarketcap.com to get a page
+# Grabs volume, price and circulating supply for top 1000 coins
+# Collects the names and links of all coins and displays on excel
+# Calculates approximate percentage of circulating supply being traded
+#       activity = circulating supply / (volume / price)
+
+print("First commit")
